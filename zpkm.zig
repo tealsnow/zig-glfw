af20c1793d34bb985283cb81093b1873bf8635c0
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2021-01-10 13:08:11 +0200

    Add zpkm stuff

---
+++ zpkm.zig
@@ -0,0 +1,12 @@
+const api = @import(".zpkm/api.zig");
+
+pub fn createManifest(b: *api.ManifestBuilder) void {
+    b.addLib(.{
+        .name = "glfw",
+        .entry_point = "glfw.zig",
+        .links = &[_][]const u8{
+            "C",
+            "glfw",
+        },
+    });
+}
